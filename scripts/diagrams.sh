5c3368000681f4c79dd2f996db7049758cc47bd2
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-02-09 16:24:48 +0900

    typo: correct spelling of 'environment' in variable loading comments

--- scripts/diagrams.sh
+++ scripts/diagrams.sh
@@ -30,7 +30,7 @@ fi
 
 
 ## --- Variables --- ##
-# Load from envrionment variables:
+# Load from environment variables:
 MODULE_NAME="${MODULE_NAME:-potato_util}"
 MODULE_DIR="${MODULE_DIR:-./src/${MODULE_NAME}}"
 OUTPUT_DIR="${OUTPUT_DIR:-./docs/diagrams}"
